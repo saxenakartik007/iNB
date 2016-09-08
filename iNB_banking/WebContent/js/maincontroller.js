bec050871520f2e6f1cc62bb80fc878a3b8aa6b4
sonaje_a <sonaje_a@Abhijeet-S.India.XoriantCorp.com>
2016-09-08 14:56:21 +0530

    all_push

--- iNB_banking/WebContent/js/maincontroller.js
+++ iNB_banking/WebContent/js/maincontroller.js
@@ -1,28 +1,43 @@
-var inbapp=angular.module('iNBapp',['ngRoute']);
-
+var inbapp=angular.module('iNBapp',['ngRoute','ngCookies']);
 
 function mainController($scope,$http){
 	
+	$('.basicslider').flexslider({
+        animation: "slide",
+        // animationLoop: true,
+        pauseOnHover: true,
+        controlNav: true,
+        directionNav: false,
+		smoothHeight: true,
+		slideshowSpeed: 3000,
+        start: function(slider) {
+            $('body').removeClass('loading');
+        }
+    });
+	
+	
 	//getUnregisterdUsers starts
-	$scope.getUnregisteredUsers(){
+	/*$scope.getUnregisteredUsers(){
 		$scope.unregisteredUsers=[];
-		var url=/*something*/+/unregistereduser/details;
+		var url=something+/unregistereduser/details;
 		$http.get(url).success(function(data,status){
-			angular.forEach(data.data./*something*/,function(value,key){
-				$scope.unRegisteredUsers.push(value./*something*/);
+			angular.forEach(data.data.something,function(value,key){
+				$scope.unRegisteredUsers.push(value.something);
 			})
 		})
-	};
+	};*/
 	//getUnregisteredUsers ends
 }
 
-inbapp.controller('mainController',mainController);
+inbapp.controller('MainController',mainController);
 
 inbapp.config(function($routeProvider){
 	$routeProvider
 	.when('/',{
-		controller:mainController,
-		templateUrl:index.html
+		controller : 'MainController',
+		templateUrl : 'index.html'
 	})
 	.otherwise({redirectTo:'/'})
 })
+
+
