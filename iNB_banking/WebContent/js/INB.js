a1516a1bcd12b8d374e553bac768ef61dd3e9cea
nair_r <nair_r@Radhika-N.India.XoriantCorp.com>
2016-09-08 17:18:28 +0530

    login commit from radhika

--- iNB_banking/WebContent/js/INB.js
+++ iNB_banking/WebContent/js/INB.js
@@ -10,7 +10,35 @@ inbapp.directive('ngSlider', function() {
 	}
 });
 
-function mainController($scope,$http){
+function mainController($scope,$http,$cookieStore){
+	//login admin starts
+	$scope.login=function(){
+		$http({
+			method : 'PUT',
+			url :'http://10.20.14.83:9000/admin/login',
+			headers : {
+				'Content-Type' : 'application/json',
+				'Access-Control-Allow-Origin': 'http://10.20.14.83:9000/'
+			},
+			data : {				
+				userName : $scope.uname,
+				password : $scope.password
+			}
+		}).then(function successCallback(response) {
+			if(response.data.error!=null){
+				alert("no login");
+			}
+			else{
+				alert(response.data.id);
+				$cookieStore.put('role','admin');
+				$cookieStore.put('admintoken',response.data.id)
+			}
+				
+		},function successCallback(response){
+			alert("no response");
+		});
+	};
+	//login admin ends
 	
 	//getUnregisterdUsers starts
 	/*$scope.getUnregisteredUsers(){
@@ -33,6 +61,10 @@ inbapp.config(function($routeProvider){
 		controller : 'MainController',
 		templateUrl : 'home.html'
 	})
+	.when('/login', {
+			controller: 'MainController',
+			templateUrl: 'login_html.html'
+		})
 	.otherwise({redirectTo:'/'})
 })
 
